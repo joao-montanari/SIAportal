ba0794e3df149081751386e077e0e127aeb24921
joao-montanari <joao.unipbr@gmail.com>
2023-03-14 10:49:48 -0300

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,28 @@
+import Head from "next/head"
+import Image from "next/image"
+import Link from "next/link"
+
+import Notificacoes from "./notificacoes"
+
+export default function Header() {
+    return (
+        <>
+            <div className="flex items-center h-20 w-full border border-gray-300 shadow-lg">
+                <div className="w-1/2 h-full items-center flex">
+                    <Link href={'/'}>
+                        <Image
+                            src={'/images/logo_unimetrocamp.png'}
+                            width={150}
+                            height={100}
+                            alt='Logo Unimetrocamp'
+                            className="ml-8"
+                        />
+                    </Link>
+                </div>
+                <div className="w-1/2 h-full items-center flex">
+                    <Notificacoes/>
+                </div>
+            </div>
+        </>
+    )
+}
