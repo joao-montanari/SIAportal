b100f2942905b2817ab975a2d2746550be5c375a
joao-montanari <joao.unipbr@gmail.com>
2023-05-16 16:28:05 -0300

    change gradient component Materia

--- src/logic/randomcolor.js
+++ src/logic/randomcolor.js
@@ -3,16 +3,16 @@ function RandomColor() {
     const min = 0;
 
     const gradient = [
-        'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 50%, rgba(0,212,255,1) 100%)',
-        'linear-gradient(90deg, rgba(23,36,0,1) 0%, rgba(36,121,9,1) 50%, rgba(1,255,0,1) 100%)',
-        'linear-gradient(90deg, rgba(23,36,0,1) 0%, rgba(121,112,9,1) 50%, rgba(255,252,0,1) 100%)',
-        'linear-gradient(90deg, rgba(23,36,0,1) 0%, rgba(114,9,121,1) 50%, rgba(255,0,250,1) 100%)',
-        'linear-gradient(90deg, rgba(23,36,0,1) 0%, rgba(121,9,9,1) 50%, rgba(255,0,52,1) 100%)',
-        'linear-gradient(90deg, rgba(23,36,0,1) 0%, rgba(9,109,121,1) 50%, rgba(0,220,255,1) 100%)',
-        'linear-gradient(90deg, rgba(121,59,9,1) 0%, rgba(255,132,0,1) 50%, rgba(255,209,0,1) 100%)',
-        'linear-gradient(90deg, rgba(23,36,0,1) 0%, rgba(62,121,9,1) 50%, rgba(181,255,0,1) 100%)',
-        'linear-gradient(90deg, rgba(82,9,121,1) 0%, rgba(183,0,255,1) 50%, rgba(255,0,215,1) 100%)',
-        'linear-gradient(90deg, rgba(9,121,91,1) 0%, rgba(0,255,188,1) 50%, rgba(0,255,209,1) 100%)',
+        'linear-gradient(90deg, rgba(181,80,0,1) 0%, rgba(255,132,0,1) 50%, rgba(255,209,0,1) 100%)',
+        'linear-gradient(90deg, rgba(195,186,0,1) 0%, rgba(255,209,0,1) 50%, rgba(245,255,0,1) 100%)',
+        'linear-gradient(90deg, rgba(171,0,0,1) 0%, rgba(255,55,0,1) 50%, rgba(255,115,0,1) 100%)',
+        'linear-gradient(90deg, rgba(12,171,0,1) 0%, rgba(0,255,25,1) 50%, rgba(0,255,128,1) 100%)',
+        'linear-gradient(90deg, rgba(0,171,91,1) 0%, rgba(0,255,196,1) 50%, rgba(0,250,255,1) 100%)',
+        'linear-gradient(90deg, rgba(0,18,171,1) 0%, rgba(98,0,255,1) 50%, rgba(179,0,255,1) 100%)',
+        'linear-gradient(90deg, rgba(157,0,171,1) 0%, rgba(255,0,241,1) 50%, rgba(255,0,172,1) 100%)',
+        'linear-gradient(90deg, rgba(6,171,0,1) 0%, rgba(147,255,0,1) 50%, rgba(250,255,0,1) 100%)',
+        'linear-gradient(90deg, rgba(0,130,171,1) 0%, rgba(25,0,255,1) 50%, rgba(0,255,235,1) 100%)',
+        'linear-gradient(90deg, rgba(171,0,116,1) 0%, rgba(255,0,112,1) 50%, rgba(255,0,241,1) 100%)',
     ];
 
     let tamanho = gradient.length;
